d889d037881f777ec83ca8f4d4ca942eac1d72a9
Aakiboi <aakashjeyakanthan47@gmail.com>
2024-07-10 12:13:02 -0400

    Tested Student.h. Everything works perfectly.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Node.h"
 #include "LinkedList.h"
+#include "Student.h"
 
 using namespace std;
 
@@ -26,5 +27,28 @@ int main() {
     cout << endl;
     list1.Print();
     list2.Print();
+
+/*  Testing Student.h
+    tm birthday1 = {0, 0, 0, 15, 6, 2003}; // July 15, 2000
+    Student student1("John", "Doe", "M00123456", birthday1, 3.5);
+
+    tm birthday2 = {0, 0, 0, 20, 11, 2004}; // December 20, 2002
+    Student student2("Jane", "Smith", "M00123457", birthday2, 3.8);
+
+    tm birthday3 = {0, 0, 0, 1, 0, 2005}; // January 1, 1995
+    Student student3("Alice", "Johnson", "M00123455", birthday3, 3.9);
+
+    // Print student information
+    cout << "Student 1: " << student1.GetName() << ", MNumber: " << student1.GetMNumber() << ", Age: " << student1.GetAge() << ", GPA: " << 3.5 << endl;
+    cout << "Student 2: " << student2.GetName() << ", MNumber: " << student2.GetMNumber() << ", Age: " << student2.GetAge() << ", GPA: " << 3.8 << endl;
+    cout << "Student 3: " << student3.GetName() << ", MNumber: " << student3.GetMNumber() << ", Age: " << student3.GetAge() << ", GPA: " << 3.9 << endl;
+
+    // Compare students by MNumber
+    cout << endl;
+    cout << "Comparing Students by MNumber:" << endl;
+    cout << (student1 > student2 ? student1.GetName() + " has a higher MNumber than " + student2.GetName() : student2.GetName() + " has a higher MNumber than " + student1.GetName()) << endl;
+    cout << (student1 < student3 ? student1.GetName() + " has a lower MNumber than " + student3.GetName() : student3.GetName() + " has a lower MNumber than " + student1.GetName()) << endl;
+    cout << (student2 == student3 ? student2.GetName() + " has the same MNumber as " + student3.GetName() : student2.GetName() + " has a different MNumber than " + student3.GetName()) << endl;
+    */
     return 0;
 }
